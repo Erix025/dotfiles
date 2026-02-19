3f7cf33375b6b3d20c463ea56163fe2413b62647
Xi Lin <erix025@outlook.com>
2026-02-19 12:00:30 +0800

    chore: add prereq tools and move openclaw to local zsh config

--- bootstrap/modules/10-prereq.sh
+++ bootstrap/modules/10-prereq.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 module_prereq() {
-    log "安装基础依赖 (git/curl/jq/bw/zsh/npm)"
+    log "安装基础依赖 (git/curl/jq/bw/zsh/npm/tmux/htop/btop/nvtop)"
     ensure_cmd git
     ensure_cmd curl
     ensure_cmd jq
     ensure_cmd npm
     ensure_cmd bw bitwarden-cli
     ensure_cmd zsh
+    ensure_cmd tmux
+    ensure_cmd htop
+    ensure_cmd btop
+    ensure_cmd nvtop
 }
